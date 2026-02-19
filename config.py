5a607f897d7fb37b4378dff94307e7bfff8fb0ce
noobaritra101-prog <noobaritra.101@gmail.com>
2026-02-19 20:17:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,7 @@ OWNER_ID = 5716292610  # 🔴 REPLACE THIS with your actual Telegram User ID!
 
 ADMIN_GROUP_ID = -1003778617867      
 AUCTION_CHANNEL_ID = -1003804045160  
-BID_LOG_GROUP_ID = -1003778617867    # Logs will go to the Admin group now
+BID_LOG_GROUP_ID = -1003778617867    
+
+# 🐘 PostgreSQL Database URL (Supabase)
+DATABASE_URL = "postgresql://postgres.kdjyzdmobuyiprczweaj:YA7oxh4AOuUlxBmZ@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres"
