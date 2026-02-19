b4d8b217b864e8321aed39a6617a1a1e844ca477
noobaritra101-prog <noobaritra.101@gmail.com>
2026-02-20 03:03:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
+# Bot configuration
 BOT_TOKEN = "8230757363:AAEMVebvcLJW6KTbKkqkHwlq3udDe-pPjxQ"
 
 OWNER_ID = 5716292610  # 🔴 REPLACE THIS with your actual Telegram User ID!
 
+# Group and Channel IDs
 ADMIN_GROUP_ID = -1003778617867      
 AUCTION_CHANNEL_ID = -1003804045160  
 BID_LOG_GROUP_ID = -1003778617867    
+PUBLIC_GROUP_ID = -1003778617867     # 🔴 NEW: Replace with your Discussion Group ID for Force Join
 
-# 🐘 PostgreSQL Database URL (Supabase)
+# Links for buttons
+CHANNEL_LINK = "https://t.me/shane_auction" # 🔴 Replace with actual link
+GROUP_LINK = "https://t.me/shane_auc"     # 🔴 Replace with actual link
+
+# Database
 DATABASE_URL = "postgresql://postgres.kdjyzdmobuyiprczweaj:YA7oxh4AOuUlxBmZ@aws-1-ap-southeast-1.pooler.supabase.com:6543/postgres"
+
