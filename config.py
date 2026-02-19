ee376b7409ea9de7d630f2ed4637b3083fa6593c
noobaritra101-prog <noobaritra.101@gmail.com>
2026-02-19 13:32:08 +0530

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-# config.py
-TOKEN = "8368723938:AAGRp6Qw0m1hhuXFoCMm_EsweIyhoRAhh84"
-DATABASE_URL = "postgresql://axb:h_9dhhH5KF_5c0xumLMziA@axb-bots-12453.jxf.gcp-asia-south1.cockroachlabs.cloud:26257/defaultdb?sslmode=require"
-ADMIN_IDS = [5716292610, 7708811819]
