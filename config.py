a8b894c2a8beed9f84fd522ead493ecdead66518
noobaritra101-prog <noobaritra.101@gmail.com>
2026-02-19 16:02:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ BOT_TOKEN = "8230757363:AAEMVebvcLJW6KTbKkqkHwlq3udDe-pPjxQ"
 WELCOME_ANIMATION_URL = "https://media.giphy.com/media/your_gif_id/giphy.gif" # Replace with valid URL or Telegram file_id
 ADMIN_GROUP_ID = -1002790195961    # Replace with your Admin Group ID
 AUCTION_CHANNEL_ID = -1002642871979 # Replace with your Auction Channel ID
+BID_LOG_GROUP_ID = -1001122334455 # NEW: Replace with your Bid Log Group ID
